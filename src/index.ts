50f946ae6bed5fb2b7071997fc9ea82bd5015e2f
Akash Sharma <122460958+sharmaakash-198@users.noreply.github.com>
2026-02-26 00:36:36 +0530

    Use environment variable for server port

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-// import { pool } from "./db";
 import identifyRouter from "./routes/identify";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use("/", identifyRouter);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
